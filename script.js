3659964823982ad2531c938c877d8755c4538e37
Yosuke Tanaka <yosket87@gmail.com>
2015-08-22 19:40:17 +0900

    backgroundChangerのリファクタリング

--- script.js
+++ script.js
@@ -2,26 +2,34 @@
 	'use strict';
 
 	$(function() {
-		backgroundChanger();
+		if ($('.login-panel').length) {
+			backgroundChanger();
+		}
 	});
 
 	function backgroundChanger() {
-		var elm = $('.login-panel');
-		var bg1 = $('<div>').addClass('bg bg-1 active');
-		var bg2 = $('<div>').addClass('bg bg-2');
-		elm.append(bg1).append(bg2);
-		var bg = $('.bg');
-		setInterval(function() {
+		var activeClass, bg;
+		backgroundChangerInit();
+		setInterval(changeBackgroundClass, 3000);
+
+		function backgroundChangerInit() {
+			activeClass = 'active';
+			var elm = $('.login-panel');
+			var bg1 = $('<div>').addClass('bg bg-1 active');
+			var bg2 = $('<div>').addClass('bg bg-2');
+			elm.append(bg1).append(bg2);
+			bg = $('.bg');
+		}
+
+		function changeBackgroundClass() {
 			bg.each(function() {
-				if ($(this).hasClass('active')) {
-					$(this).removeClass('active');
-					return;
-				}
-				if (!$(this).hasClass('active')) {
-					$(this).addClass('active');
-					return;
+				var thisElm = $(this);
+				if (thisElm.hasClass(activeClass)) {
+					thisElm.removeClass(activeClass);
+				} else {
+					thisElm.addClass(activeClass);
 				}
 			});
-		}, 3000);
+		}
 	}
 })();
