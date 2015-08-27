dcc4006e872027dbc13912709a9824e287a4e037
Yosuke Tanaka <yosket87@gmail.com>
2015-08-28 01:02:59 +0900

    gulp-imagemin導入

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var copy = require('gulp-copy');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('copy', function() {
 	return gulp.src([
@@ -12,3 +14,19 @@ gulp.task('copy', function() {
 		base: 'src/client'
 	}).pipe(gulp.dest('dest/client'));
 });
+
+var imagePath = {
+	src: 'src/client/app/img/*.+(jpg|jpeg|png|gif|svg)',
+	dist: 'dest/client/app/img/'
+};
+
+gulp.task('imagemin', function() {
+	return gulp.src(imagePath.src)
+		.pipe(imagemin())
+		.pipe(gulp.dest(imagePath.dist));
+});
+
+gulp.task('default', [
+	'copy',
+	'imagemin'
+]);
